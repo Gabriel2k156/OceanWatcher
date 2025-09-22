abb8e5dccf670edb0697a23a1b5e788c3dc17994
Edn.Santos <135707128+EdBLK@users.noreply.github.com>
2025-09-22 16:17:58 -0300

    Update app.py
    arquivo Wendel parte python

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def dados():
     dados = {
         'plastico': [17.31,81.79, 57.54, 55.28, round(random.uniform(1,50),2)],
         'outros': [0.09, 5.05, 4.19, 3.30, round(random.uniform(1,50),2)],
-        'cigarro': [12.8, 13, 12.9, 13.1, round(random.uniform(1,50),2)] 
+
     }
     return jsonify(dados)
 
