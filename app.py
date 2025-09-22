05f10575b05cce99e8cea09c6ddb1d2c15596e02
Gabriel Marques <gabrielmarx04@gmail.com>
2025-09-22 16:53:49 -0300

    Atualização no gráfico
    Alterei o valor da coluna "Outros" de 2021 para 1  para que a coluna não fique oculta do gráfico. Valor real = 0,09T

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def homepage():
 def dados():
     dados = {
         'plastico': [17.31,81.79, 57.54, 55.28, round(random.uniform(1,100),2)],
-        'outros': [0.09, 5.05, 4.19, 3.30, round(random.uniform(1,100),2)],
+        'outros': [1, 5.05, 4.19, 3.30, round(random.uniform(1,100),2)], # O valor de real 2021 é 0.09 toneladas, foi alterado para 1 para a coluna não ficar oculta
 
     }
     return jsonify(dados)
